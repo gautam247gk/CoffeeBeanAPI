3a2976c1a4cbcb4dc6a82a8d691b972ca6d3dcfd
gautam247gk <gautam247gk@gmail.com>
2025-01-31 21:57:46 +0000

    Added models % Nuget packages

---
+++ CoffeeBeanAPI/Models/Bean.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeBeanAPI.Models
+{
+    public class Bean
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public int IndexNumber { get; set; }
+
+        public bool IsBOTD { get; set; }
+
+        [Required]
+        public decimal Cost { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Colour { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Country { get; set; }
+    }
+
+}
+
+
---
+++ CoffeeBeanAPI/Models/BeanOfTheDay.cs
@@ -0,0 +1,26 @@
+﻿using CoffeeBeanAPI.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CoffeeBeanAPI.Models
+{
+    public class BeanOfTheDay
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid BeanId { get; set; }
+
+        [Required]
+        [Column(TypeName = "Date")]
+        public DateTime SelectedDate { get; set; }
+
+        [ForeignKey("BeanId")]
+        public Bean Bean { get; set; }
+    }
+
+}
+
+
